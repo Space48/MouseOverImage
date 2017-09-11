584d4ccdc585ba6c589357f2055c37aa35fcfa4e
Raul E Watson <raul@space48.com>
2017-09-11 13:02:45 +0100

    Fix OverImage not showing because js issues

--- view/frontend/web/js/mouseover-image.js
+++ view/frontend/web/js/mouseover-image.js
@@ -2,18 +2,19 @@ define(['jquery'], function($) {
 
     return function (config, element) {
         var $el = $(element);
-        var altSrc = $el.data('alt-src');
-        var src = $el.prop('src');
+        var $img = $el.find('img[data-alt-src]');
+        var altSrc = $img.data('alt-src');
+        var src = $img.prop('src');
 
         if (altSrc !== '') {
             $el.on('mouseenter', function () {
-                $el.prop({
+                $img.prop({
                     src: altSrc
                 });
             });
 
             $el.on('mouseleave', function () {
-                $el.prop({
+                $img.prop({
                     src: src
                 });
             });
